e5f635180033dab18190cc04a0279d2033fc6ce4
ShobanaMuthiah <shobanam222@gmail.com>
2024-04-17 13:02:45 -1200

    completed

---
+++ script.js
@@ -0,0 +1,43 @@
+const div=document.createElement('div');
+const heading=document.createElement('h1');
+
+div.setAttribute('class','dis');
+heading.setAttribute('class','head');
+
+heading.innerHTML="CallBack Hell";
+setTimeout(()=>{
+    div.innerHTML="10"
+    setTimeout(()=>{
+        div.innerHTML="9"
+        setTimeout(()=>{
+            div.innerHTML="8"
+            setTimeout(()=>{
+            div.innerHTML="7"
+            setTimeout(()=>{
+                div.innerHTML="6"
+                setTimeout(()=>{
+                    div.innerHTML="5"
+                    setTimeout(()=>{
+                        div.innerHTML="4"
+                        setTimeout(()=>{
+                            div.innerHTML="3"
+                            setTimeout(()=>{
+                                div.innerHTML="2"
+                                setTimeout(()=>{
+                                    div.innerHTML="1"
+                                    setTimeout(()=>{
+                                        div.innerHTML="Happy Independence Day"
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+            
+        },1000)
+    },1000)
+},1000)
+
+document.body.append(heading,div)
